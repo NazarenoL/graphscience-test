c7e6aeca7eb47b731965a2633a2c33c333295a64
Nazareno Lorenzo <nazarenolorenzo@gmail.com>
2013-10-04 08:17:31 -0300

    Charset fix

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -3,6 +3,7 @@
     <head>
         <title><?php echo $title;?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
         <!-- Bootstrap -->
         <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
         <link href="<?php echo base_url();?>assets/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
